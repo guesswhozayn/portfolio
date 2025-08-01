2142019f9e285eb5de85d7f08a7da307c571c278
zayn <ltlehro@gmail.com>
2025-08-01 14:37:05 +0500

    updated email

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -2,8 +2,8 @@ const Email = () => {
   return (
     <div className="max-md:hidden flex fixed right-10 bottom-0 origin-right rotate-90 transform items-center gap-4 text-white cursor-pointer">
       <h2>
-        <a href="mailto:zainulabidin74@outlook.com">
-          zainulabidin74@outlook.com
+        <a href="mailto:z4yndev@gmail.com">
+          z4yndev@gmail.com
         </a>
       </h2>
       <span className="w-16 h-[1px] bg-gray-500"></span>
