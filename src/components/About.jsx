7e06788064903346b041746a1bf7ed3c9ae70622
zayn <ltlehro@gmail.com>
2025-08-19 12:26:26 +0500

    updated pfp

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import myImage from "../assets/img/img.jpg";
+import myImage from "../assets/img/img.PNG";
 
 const About = ({ id }) => {
   const containerVariants = {
